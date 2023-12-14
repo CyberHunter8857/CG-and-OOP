1c3a83d8b1a06c3d8010338ac41ff3f61b7c441a
ecpracticles <136753085+ecpracticles@users.noreply.github.com>
2023-12-15 00:26:04 +0530

    Add files via upload

--- OOP Ass4 Filehandling.cpp
+++ OOP Ass4 Filehandling.cpp
@@ -1,47 +1,47 @@
-
-// #include<iostream>
-// #include<fstream>
-// using namespace std;
-// int main(){
-//     fstream file;
-//     string s;
-//     string x;
-//     file.open("data.txt",ios::out);
-//     cout<<"Entr Data -"<<endl;
-//     getline(cin,s);
-//     file<<s;
-//     file.close();
-//     file.open("data.txt",ios::in);
-//     cout<<"INFO is :"<<endl;
-//     file>>x;
-//     cout<<x;
-//     file.close();
-//     return 0;
-// }
-
-#include<iostream>
-#include<fstream>
+ #include<iostream> 
+#include<fstream> 
 using namespace std;
-int main(){
-    fstream file;
-    string s;
-    string x;
-	int n,i;
-	cout<<"Enter size"<<endl;
-	cin>>n;
-    file.open("data.txt",ios::out);
-	for(i=0;i<n;i++){
-	    cout<<"Entr Data -"<<endl;
-    	getline(cin,s);
-    	file<<s;
-	}
-    file.close();
-    file.open("data.txt",ios::in);
-	for(i=0;i<n;i++){
-		cout<<"INFO is :"<<endl;
-    	file>>x;
-    	cout<<x;
-	}
-    file.close();
-    return 0;
+
+class Student
+{
+char Name[20]; 
+int Rollno;
+public:
+void accept()
+{
+cin>>Rollno; 
+cin>>Name; 
 }
+void display()
+{
+cout<<"\n Rollno:"<<Rollno; 
+cout<<"\n Name:"<<Name; 
+}
+};
+int main()
+{
+Student s[5]; 
+fstream f;
+int i,n;
+f.open("Ass4.txt");
+cout<<"\n How many student information do you need to store?"; 
+cin>>n;
+cout<<"\n Enter information of student in this format(ROLLNO / NAME)"; 
+for(i=1;i<=n;i++)
+{
+cout<<"\n Enter information of: "<<i<<" Student"<<endl; 
+s[i].accept();
+f.write((char*)&s[i],sizeof s[i]);
+}
+f.close();
+f.open("Ass4.txt",ios::in);
+cout<<"\n Information of Student is as follows:"; 
+for(i=1;i<=n;i++)
+{
+f.read((char*)&s[i],sizeof s[i]); 
+s[i].display();
+}
+f.close();
+return 0;
+}
+
